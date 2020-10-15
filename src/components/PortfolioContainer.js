0df0a222c3bdc1e862009895ee9adb203912b82c
Kennadee <kennadee.ray@gmail.com>
2020-10-15 14:40:38 -0500

    nav links functional

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,15 +1,26 @@
 import React, {useState} from 'react';
 import Navigation from './Header/Navigation';
 import Header from './Header/index';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Portfolio from './Pages/Portfolio';
+import Resume from './Pages/Resume';
+
 import '../App.css';
 
-function Portfolio() {
+function PortfolioContainer() {
     const [currentPage, handlePageChange] = useState('Home');
   
     const renderPage = () => {
       // Add a switch statement that will return the appropriate component of the 'currentPage'
       // YOUR CODE HERE
-      //
+      switch (currentPage) {
+          case 'About': return <About/>;
+          case 'Contact': return <Contact/>;
+          case 'Portfolio': return <Portfolio/>;
+          case 'Resume': return <Resume/>;
+          default: return <About/>
+      }
     };
   
     return (
@@ -18,14 +29,12 @@ function Portfolio() {
         <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
         <div>
           {
-            // Render the component returned by 'renderPage()'
-            // YOUR CODE HERE
-            //
+           renderPage()
           }
         </div>
       </div>
     );
   }
   
-  export default Portfolio;
+  export default PortfolioContainer;
   
