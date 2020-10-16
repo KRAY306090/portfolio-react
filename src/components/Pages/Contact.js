a466b684a27969327e1b320fe4b1fa9e94d3c04a
Kennadee <kennadee.ray@gmail.com>
2020-10-15 22:01:37 -0500

    handle submit function added

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Contact = () => (
-    <div>
-        <h2>Contact</h2>
-        <p>
-           Please fill out the contact form below so I can get in touch.
-        </p>
-    </div>
-);
+function Contact() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        setFormState({ ...formState, [e.target.name]: e.target.value })
+    }
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+    return (
+        <section className="container">
+            <h2 className="section-title">Contact me</h2>
+            <form id="contact-form">
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" name="name" defaultValue={name} onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="email">Email address:</label>
+                    <input type="email" name="email" defaultValue={email} onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        </section>
+    )
+}
 
 export default Contact;
