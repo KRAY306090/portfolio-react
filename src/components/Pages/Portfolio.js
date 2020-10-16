b22c6be5c9c84e8729ffccfa9e11a7583c001bc2
Kennadee <kennadee.ray@gmail.com>
2020-10-15 19:52:22 -0500

    card renders

--- src/components/Pages/Portfolio.js
+++ src/components/Pages/Portfolio.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import Project from '../Project/index';
+import Wrapper from '../Wrapper';
+import projects from '../Project/projects.json';
 
 
-const Portfolio = () => (
-    <div>
-        <h2>My Work</h2>
-        <p>
-          Projects here
-        </p>
-    </div>
-);
+function Portfolio() {
+   return (
+     <Wrapper>
+       <h2 className="section-title"></h2>
+       {
+         projects.map(project => (
+           <Project
+           id={project.id}
+           image={project.image}
+           name={project.name}
+           github={project.github}
+           deployed={project.deployed}
+           />
+         ))
+       }
+
+
+     </Wrapper>
+   );
+   }
 
 export default Portfolio;
