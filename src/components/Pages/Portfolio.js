e2bcd63c5f0f966b363a069e061b32bf11d64a3b
Kennadee <kennadee.ray@gmail.com>
2020-10-15 21:44:27 -0500

    project card styles

--- src/components/Pages/Portfolio.js
+++ src/components/Pages/Portfolio.js
@@ -7,10 +7,12 @@ import projects from '../Project/projects.json';
 function Portfolio() {
    return (
      <Wrapper>
-       <h2 className="section-title"></h2>
+       <h2 className="section-title">Projects</h2>
+       <div class="container">
+       <div className="row">
        {
          projects.map(project => (
-           <Project
+           <Project 
            id={project.id}
            image={project.image}
            name={project.name}
@@ -19,7 +21,8 @@ function Portfolio() {
            />
          ))
        }
-
+       </div>
+       </div>
 
      </Wrapper>
    );
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -2,13 +2,13 @@ import React from "react";
 
 function Project(props) {
     return (
-        <div className="card">
+        <div className="card m-3">
             <img class="card-img-top" src={props.image}></img>
             <div class="card-body">
                 <h5 className="card-title">{props.name}</h5>
                 
-                <a href={props.github}>Github</a>
-                <a href={props.deployed}>Deployed Application</a>
+                <a href={props.github}><i className="fab fa-github fa-lg  mr-md-5 mr-3 fa-2x"> </i></a>
+                <a href={props.deployed}><i class="fas fa-external-link-alt fa-2x"></i></a>
             </div>
 
         </div>
