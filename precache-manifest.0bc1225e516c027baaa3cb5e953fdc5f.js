2fbeeee2965a5d5f73443e0fee8b92a9b782d837
Kennadee <kennadee.ray@gmail.com>
2020-10-18 19:32:31 -0500

    Updates

---
+++ precache-manifest.0bc1225e516c027baaa3cb5e953fdc5f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e72aef3da0f4e0c614b2d58b12ac1200",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "5a1ab95d103e1d060050",
+    "url": "/portfolio-react/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "f08815eb7c9670300662",
+    "url": "/portfolio-react/static/css/main.d8d275ef.chunk.css"
+  },
+  {
+    "revision": "5a1ab95d103e1d060050",
+    "url": "/portfolio-react/static/js/2.7b132267.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/portfolio-react/static/js/2.7b132267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f08815eb7c9670300662",
+    "url": "/portfolio-react/static/js/main.ec8301f0.chunk.js"
+  },
+  {
+    "revision": "2f26d0aed242c262185b",
+    "url": "/portfolio-react/static/js/runtime-main.a4de92d3.js"
+  },
+  {
+    "revision": "65f5182802cc703ef06255402ec122ba",
+    "url": "/portfolio-react/static/media/Resume.65f51828.pdf"
+  },
+  {
+    "revision": "2856240b4c781c2a7b4cdf5a65652ec3",
+    "url": "/portfolio-react/static/media/profile.2856240b.jpg"
+  }
+]);
